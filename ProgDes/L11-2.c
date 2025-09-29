8c5ecea4c341b5be44a9686a64c7ae0148e31d5f
Caetanoblanke <Caetano.b.seixas@gmail.com>
2025-09-28 23:35:11 -0300

    Add files via upload

---
+++ ProgDes/L11-2.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+
+int main(){
+
+    char ch;
+    int linhas=0;
+
+    FILE *file = fopen("entrada.txt","r");
+    if (file == NULL)
+    {
+        printf("Erro ao abrir arquivo\n");
+        return 1;
+    }
+
+    printf("Lendo o arquivo...\n");
+    while ((ch = fgetc(file)) != EOF )
+    {
+        if (ch == '\n')
+        {
+            linhas++;
+        }
+        
+    }
+
+    fclose(file);
+
+    printf("O arquivo tem %d Linhas",linhas);
+    
+}
