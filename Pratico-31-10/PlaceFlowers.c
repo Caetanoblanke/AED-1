0732418e4f02f4f18369955433ec34578c3b4e06
Caetanoblanke <Caetano.b.seixas@gmail.com>
2025-11-02 16:35:31 -0300

    Add files via upload

---
+++ Pratico-31-10/PlaceFlowers.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+bool canPlaceFlowers(int* flowerbed, int flowerbedSize, int n);
+
+int main(){
+    int flowerbedSize;
+    printf("type the flowerbed size: ");
+    scanf("%d" ,&flowerbedSize);
+
+    int *flowerbed = malloc(sizeof(int) * flowerbedSize);
+    if (!flowerbed)
+    {
+        printf("error\n");
+        return -1;
+    }
+
+    printf("Type 0 to empty 1 to not empty: \n");
+    for(int i = 0; i < flowerbedSize; i++)
+    {
+        scanf("%d" ,&flowerbed[i]);
+    }
+
+    int n;
+    printf("Insert how many flower you want add: ");
+    scanf("%d", &n);
+
+    canPlaceFlowers(flowerbed,flowerbedSize,n);
+    
+}
+
+
+bool canPlaceFlowers(int* flowerbed, int flowerbedSize, int n)
+{
+   for (int i = 0; i < flowerbedSize && n>0; i++)
+    {
+        
+        if (flowerbed[i]==0 && (i==0 || flowerbed[i-1] == 0) && (i==flowerbedSize-1 || flowerbed[i+1]==0))
+        {
+            n--;
+            flowerbed[i] = 1;
+        }
+    }
+
+    if (n==0)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}  
+
